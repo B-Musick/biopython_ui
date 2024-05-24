3b80a65ef1e2a451bd7eb097d22c127d63e3ef56
Brendan Musick <bmuze1@gmail.com>
2024-05-24 11:48:38 -0600

    #6 - Added modal capability

---
+++ src/components/Modal.tsx
@@ -0,0 +1,47 @@
+import ReactDOM from 'react-dom';
+import { useEffect, useRef } from 'react';
+import React from 'react';
+import { IoIosCloseCircle } from "react-icons/io";
+
+type ModalProps = {
+    setShowModal: any, 
+    children: any, 
+    actionBar: any, 
+    classes: string, 
+    childClasses: string
+}
+
+const Modal: React.FC<ModalProps> = ({ setShowModal, children, classes, childClasses }) => {
+    const modal = useRef(document.createElement('div'))
+    useEffect(()=>{
+        const root = document.querySelector('.modal');
+        root?.appendChild(modal.current)
+    
+        // When modal being removed, need to remove the class
+        return ()=>{
+            root?.removeChild(modal.current)
+        }
+    }, [])
+    
+    const handleModalClose = () => {
+        setShowModal(false);
+    }
+
+    return ReactDOM.createPortal(
+        <div data-testid="modal" className='fixed flex left-0 top-0 h-full w-full justify-center items-center'>
+            <div className="fixed bg-gray-400 opacity-80 h-full w-full" onClick={handleModalClose}></div>
+            <div className={`w-fit h-fit rounded-3xl z-[1] ${classes} relative`}>
+                <div className={`absolute right-1 top-0`}>
+                    <button className='modalButton text-2xl mt-1' onClick={handleModalClose}><IoIosCloseCircle /></button>
+                </div>
+                <div className={`flex flex-col justify-between h-fit ${childClasses}`}>
+                    {children}
+                </div>
+            </div>
+        </div>,
+        
+        modal.current
+    )
+}
+
+export default Modal;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import Modal from "../components/Modal";
+
 export const HomePage = () => {
+    const [showModal, setShowModal] = useState(false);
+    
+    let actionBar = <div><button>action button</button></div>
+
+    let modal = <Modal 
+    classes="bg-white px-2" 
+    childClasses="" 
+    className="z-[0]" 
+    setShowModal={setShowModal} 
+    actionBar={actionBar}>
+        <div className="w-200 h-200">Place Modal Data Here</div>
+    </Modal>;
+
     return (
-        <div>Home</div>
+        <>
+            <div>Home</div>
+            <button onClick={()=>setShowModal(true)}>Show Modal</button>
+            {showModal && modal}
+        </>
     )
 }
