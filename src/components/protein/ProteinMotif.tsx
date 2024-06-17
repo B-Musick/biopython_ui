42b881cab45cb0e35b671049528fe7c74fc8981c
Brendan Musick <bmuze1@gmail.com>
2024-06-17 15:14:43 -0600

    #24 - Can add protein to form from saved, type in motif and return locations

---
+++ src/components/protein/ProteinMotif.tsx
@@ -0,0 +1,83 @@
+import { useOutletContext } from "react-router-dom";
+import ErrorMessage from "../forms/ErrorMessage";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+
+export default function ProteinMotif() {
+    const { watch, register, handleSubmit, formState: {errors}, getValues, setValue } = useForm();
+
+    const [selectedRecord, setSelectedRecord] = useOutletContext();
+    const [matchLocations, setMatchLocations] = useState([])
+
+    const onSubmit = (data) => {
+        console.log(data)
+        setSelectedRecord({})
+        console.log(data.sequence)
+        createMotifRegex(data.motif, data.sequence)
+    }
+
+    useEffect(()=>{
+        setValue('sequence', selectedRecord.sequence)
+    }, [selectedRecord])
+
+    const createMotifRegex = (motif: string, sequence: string) =>{
+        let allExceptBrace = /{[ARNDCEQGHILKMFPSTWYV]}/g
+        let possibleProteins = "ARNDCEQGHILKMFPSTWYV"
+        let matchedBraces = [...motif.matchAll(allExceptBrace)]
+        matchedBraces.forEach(brace=>{
+            console.log(brace)
+            let letter = brace[0][1]
+            let proteinList = possibleProteins.replace(letter, '')
+            // console.log(letter)
+            // Negative lookahead assertion - https://stackoverflow.com/questions/29859968/how-to-match-all-alphabet-except-few
+            motif = motif.replace(brace[0], `[${proteinList}]`)
+        })
+        console.log(motif.trim())
+        console.log(sequence)
+        let regex = new RegExp(motif, 'g');
+        let matches = [...sequence.matchAll(regex)]
+        let locations = []
+
+        // console.log(matches)
+        matches.forEach(match=>{
+            locations.push(match.index+1)
+            console.log(match.index)
+        })
+        setMatchLocations(locations)
+
+        // motif.split('').forEach((letter)=>{
+        //     if(letter == '{') { 
+        //         // squareEitherBrace = true;
+
+        //     }
+        // })
+    }
+
+    return (
+        <div className="w-full flex flex-col items-center">
+        <form onSubmit={handleSubmit(onSubmit)} className="z-[2] flex flex-col w-1/4 h-fit rounded-xl p-4 bg-gray-200 items-center  bg-slate-800">
+            {/* Place a dropdown of existing motifs */}
+            <div className="flex flex-col w-full">
+                <input 
+                    {...register('motif', { required:true } )} 
+                        aria-invalid={errors.motif ? "true" : "false"
+                    }
+                    placeholder="Motif" 
+                    className={`rounded-lg pl-2 ${errors.motif ? 'border-2 border-rose-500': ''}`}
+                />
+                {errors.entry_name?.type === 'required' && <ErrorMessage message={"Protein sequence required"} />}
+            </div>
+
+            <div className="flex flex-col w-full">
+                <textarea {...register('sequence', { required:true, pattern: /^[ARNDCEQGHILKMFPSTWYV]+$/  })}
+                aria-invalid={errors.sequence ? "true" : "false"} placeholder="Sequence" className="rounded-lg pl-2  mt-2 overflow-scroll p-1 w-auto" />
+                {errors.sequence?.type === 'required' && <ErrorMessage message="Sequence is required" />}
+                {errors.sequence?.type === 'pattern' && <ErrorMessage message="Should only include nucleotide" />}
+            </div>
+
+            <input type="submit" className="form-button bg-purple-300 rounded w-3/4 m-1 mt-2"/>
+        </form>
+        <div>{matchLocations.join(' ')}</div>
+    </div>
+    )
+}
--- src/pages/ProteinPage.tsx
+++ src/pages/ProteinPage.tsx
@@ -1,22 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
-import { createProtein, deleteProtein, getProteins } from "../api/protein";
+import { deleteProtein, getProteins } from "../api/protein";
 import Table from "../components/Table";
 import useDeleteMutation from "../hooks/useDeleteMutation";
-import { MdDeleteForever } from "react-icons/md";
-import { useForm } from "react-hook-form";
-import { SwissProtRecord } from "../lib/types";
-import { VscErrorSmall } from "react-icons/vsc";
-import ErrorMessage from "../components/forms/ErrorMessage";
-import useSaveMutation from "../hooks/useSaveMutation";
+import { MdDeleteForever, MdUpload } from "react-icons/md";
+import SectionNavigation from "../components/SectionNavigation";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function ProteinPage(){
     const [deleteItem] = useDeleteMutation(deleteProtein, ()=>{}, 'proteins')
-    const { watch, register, handleSubmit, formState: {errors}, getValues, setValue } = useForm();
-    const [save, saveIcon] = useSaveMutation(createProtein, ()=>{}, 'proteins')
+    const [selectedRecord, setSelectedRecord] = useState({})
 
-    const onSubmit = (data: SwissProtRecord) => {
-        save(data)
-    }
+    const sectionItems = [
+        {title: 'Add Protein', url: '/protein/add', icon: null} as NavLinkInfo,
+        {title: 'Motif', url: '/protein/motif', icon: null} as NavLinkInfo,
+    ]
 
     const proteinsQuery = useQuery({
         queryKey: ["proteins"],
@@ -27,11 +25,12 @@ function ProteinPage(){
         , {name: "action", button: true, cell: (row) => 
         (
             <div>
-            <button
-              onClick={() => deleteItem(row.id)}
-            >  
-              <MdDeleteForever className="text-red-500 w-5 h-5" />
-            </button>
+                <button onClick={()=>setSelectedRecord(row)}><MdUpload className="text-green-500 w-5 h-5"/></button>
+                <button
+                onClick={() => deleteItem(row.id)}
+                >  
+                <MdDeleteForever className="text-red-500 w-5 h-5" />
+                </button>
             </div>
     
           )
@@ -47,38 +46,18 @@ function ProteinPage(){
     }
 
     return (
-        <div className={`flex flex-col items-center w-full z-[4]`}>
-        <h2 className="text-white font-bold text-2xl p-2 text-center">Proteins</h2>
-        <div className="w-5/6 bg-slate-700 rounded-xl">
-            <Table classes="" theme="dark" cols={columns} rows={rows} hiddenCols={[]} sortable={[]} onRowClick={function (): {} {
-                throw new Error("Function not implemented.")
-            } } rowConditionals={undefined} title={""} fixedHeader={false} />
-        </div>
-
-        <div className="h-full w-full flex flex-col items-center">
-            <form onSubmit={handleSubmit(onSubmit)} className="z-[2] flex flex-col w-1/4 h-fit rounded-xl p-4 bg-gray-200 items-center  bg-slate-800">
-                {/* Place a dropdown of existing motifs */}
-                <div className="flex flex-col w-full">
-                    <input 
-                        {...register('entry_name', { required:true } )} 
-                            aria-invalid={errors.entry_name ? "true" : "false"
-                        }
-                        placeholder="Protein Name" 
-                        className={`rounded-lg pl-2 ${errors.entry_name ? 'border-2 border-rose-500': ''}`}
-                    />
-                    {errors.entry_name?.type === 'required' && <ErrorMessage message={"Protein sequence required"} />}
+        <div className={`flex w-full z-[4]`}>
+            <SectionNavigation sections={sectionItems} />
+            <div className="w-full flex flex-col items-center">
+                <Outlet context={[selectedRecord, setSelectedRecord]}/>
+                <h2 className="text-white font-bold text-2xl p-2 text-center">Proteins</h2>
+                
+                <div className="w-5/6 bg-slate-700 rounded-xl">
+                    <Table classes="" theme="dark" cols={columns} rows={rows} hiddenCols={[]} sortable={[]} onRowClick={function (): {} {
+                        throw new Error("Function not implemented.")
+                    } } rowConditionals={undefined} title={""} fixedHeader={false} />
                 </div>
-
-                <div className="flex flex-col w-full">
-                    <textarea {...register('sequence', { required:true, pattern: /^[ARNDCEQGHILKMFPSTWYV]+$/  })} 
-                    aria-invalid={errors.sequence ? "true" : "false"} placeholder="Sequence" className="rounded-lg pl-2  mt-2 overflow-scroll p-1 w-auto" />
-                    {errors.sequence?.type === 'required' && <ErrorMessage message="Sequence is required" />}
-                    {errors.sequence?.type === 'pattern' && <ErrorMessage message="Should only include nucleotide" />}
-                </div>
-
-                <input type="submit" className="form-button bg-purple-300 rounded w-3/4 m-1 mt-2"/>
-            </form>
-        </div>
+            </div>
     </div>
     )
 }
